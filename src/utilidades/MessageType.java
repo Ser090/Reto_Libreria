6a1c3332d874497fa97f74c756b3d09167a5e1a9
Urkopf <144771867+Urkopf@users.noreply.github.com>
2024-11-07 19:28:00 +0100

    Ultimo push

--- src/utilidades/MessageType.java
+++ src/utilidades/MessageType.java
@@ -12,33 +12,69 @@ import java.io.Serializable;
  * Cada tipo de mensaje indica un estado o una solicitud específica, facilitando
  * la identificación y manejo de cada caso.
  *
- * Tipos de mensajes: - `SIGN_IN_REQUEST`: solicitud para iniciar sesión. -
- * `SIGN_UP_REQUEST`: solicitud para registrarse. - `OK_RESPONSE`: respuesta
- * indicando que la operación fue exitosa. - `SIGNIN_ERROR`: error durante el
- * inicio de sesión. - `SIGNUP_ERROR`: error durante el registro. -
- * `BAD_RESPONSE`: respuesta indicando que ocurrió un error desconocido. -
- * `LOGIN_OK`: inicio de sesión exitoso. - `LOGIN_EXIST_ERROR`: error indicando
- * que el login ya existe. - `SQL_ERROR`: error de base de datos SQL. -
- * `CONNECTION_ERROR`: error de conexión al servidor. - `SERVER_ERROR`: error
- * interno del servidor. - `NON_ACTIVE`: el usuario está inactivo.
+ * Tipos de mensajes: - `SIGN_IN_REQUEST`: Solicitud para iniciar sesión. -
+ * `SIGN_UP_REQUEST`: Solicitud para registrarse. - `OK_RESPONSE`: Respuesta
+ * indicando que la operación fue exitosa. - `SIGNIN_ERROR`: Error durante el
+ * inicio de sesión. - `SIGNUP_ERROR`: Error durante el registro. -
+ * `BAD_RESPONSE`: Respuesta indicando que ocurrió un error desconocido. -
+ * `LOGIN_OK`: Inicio de sesión exitoso. - `LOGIN_EXIST_ERROR`: Error indicando
+ * que el nombre de usuario ya existe. - `SQL_ERROR`: Error de base de datos
+ * SQL. - `CONNECTION_ERROR`: Error de conexión al servidor. - `SERVER_ERROR`:
+ * Error interno del servidor. - `NON_ACTIVE`: El usuario está inactivo.
  *
  * Este enum facilita la comunicación y el control de errores en el sistema de
  * autenticación.
  *
- * @author Urko
+ * @autor Urko
  */
 public enum MessageType implements Serializable {
 
+    /**
+     * Solicitud para iniciar sesión.
+     */
     SIGN_IN_REQUEST,
+    /**
+     * Solicitud para registrarse.
+     */
     SIGN_UP_REQUEST,
+    /**
+     * Respuesta indicando que la operación fue exitosa.
+     */
     OK_RESPONSE,
+    /**
+     * Error durante el inicio de sesión.
+     */
     SIGNIN_ERROR,
+    /**
+     * Error durante el registro.
+     */
     SIGNUP_ERROR,
+    /**
+     * Respuesta indicando que ocurrió un error desconocido.
+     */
     BAD_RESPONSE,
+    /**
+     * Inicio de sesión exitoso.
+     */
     LOGIN_OK,
+    /**
+     * Error indicando que el nombre de usuario ya existe.
+     */
     LOGIN_EXIST_ERROR,
+    /**
+     * Error de base de datos SQL.
+     */
     SQL_ERROR,
+    /**
+     * Error de conexión al servidor.
+     */
     CONNECTION_ERROR,
+    /**
+     * Error interno del servidor.
+     */
     SERVER_ERROR,
+    /**
+     * El usuario está inactivo.
+     */
     NON_ACTIVE
 }
